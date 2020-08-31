caf853a5d24ea9ae4f64fdb4882f6894799cbbf9
YatharthDesai <36489137+YatharthDesai@users.noreply.github.com>
2020-08-31 06:07:57 -0400

    Unit tests added

---
+++ inventory-allocator/InventoryAllocator.test.js
@@ -0,0 +1,220 @@
+
+const allocator = require('./src/InventoryAllocator')
+
+// The first case in the example
+test('Demo test 1', () => {
+    expect(allocator({apple: 1}, [
+        {name: "owd", inventory: {apple: 1}}
+    ])).toEqual([
+                    {
+                        owd: {apple: 1}
+                    }
+                ])
+})
+
+// The second case in the example
+test('Demo test 2', () => {
+    expect(allocator({apple: 10}, [
+        {
+            name: "owd",
+            inventory: {apple: 5}
+        },
+        {
+            name: "dm",
+            inventory: {apple: 5}
+        }
+    ])).toEqual([
+                    {
+                        owd: {apple: 5}
+                    },
+                    {
+                        dm: {apple: 5}
+                    }
+                ])
+})
+
+// The third case in the example
+test('Demo test 3', () => {
+    expect(allocator({apple: 1}, [
+        {name: "owd", inventory: {apple: 0}}
+    ])).toEqual([])
+})
+
+// The fourth case in the example
+test('Demo test 4', () => {
+    expect(allocator({apple: 2}, [
+        {name: "owd", inventory: {apple: 1}}
+    ])).toEqual([])
+})
+
+// Ship multiple items from the warehouse
+test('Ship multiple items', () => {
+    expect(allocator({apple: 2, banana: 4, grapes: 2}, [
+        {name: "owd", inventory: {apple: 4, banana: 5, grapes: 4}}
+    ])).toEqual([{owd: {apple: 2, banana: 4, grapes: 2}}])
+})
+
+// Ship multiple items but one incomplete from same warehouse
+test('Ship multiple items but one incomplete', () => {
+    expect(allocator({apple: 2, banana: 4, grapes: 2}, [
+        {name: "owd", inventory: {apple: 4, banana: 3, grapes: 4}}
+    ])).toEqual([{owd: {apple: 2, grapes: 2}}])
+})
+
+// Ship multiple items but multiple incomplete from same warehouse
+test('Ship multiple items but multiple incomplete', () => {
+    expect(allocator({apple: 2, banana: 4, grapes: 5}, [
+        {name: "owd", inventory: {apple: 4, banana: 3, grapes: 4}}
+    ])).toEqual([{owd: {apple: 2}}])
+})
+
+// Ship multiple items but none complete from same warehouse
+test('Ship multiple items but multiple incomplete', () => {
+    expect(allocator({apple: 7, banana: 4, grapes: 5}, [
+        {name: "owd", inventory: {apple: 4, banana: 3, grapes: 4}}
+    ])).toEqual([])
+})
+
+// Ship multiple items from multiple warehouses
+test('Multiple items from multiple warehouses', () => {
+    expect(allocator({apple: 10, mango: 6, pineapple: 5}, [
+        {
+            name: "owd",
+            inventory: {apple: 5, mango: 3, pineapple: 3}
+        },
+        {
+            name: "dm",
+            inventory: {apple: 5, mango: 2, pineapple: 2}
+        },
+
+        {
+            name: "tm",
+            inventory: {apple: 5, mango: 2, pineapple: 2}
+        }
+
+    ])).toEqual([{
+        owd: {
+            apple: 5,
+            mango: 3,
+            pineapple: 3
+        }
+    },
+                    {
+                        dm: {
+                            apple: 5,
+                            mango: 2,
+                            pineapple: 2
+                        }
+                    },
+                    {
+                        tm: {
+                            mango: 1
+                        }
+                    }])
+})
+
+// Ship multiple items from multiple warehouses but some incomplete
+test('Multiple items multiple warehouses and some incomplete', () => {
+    expect(allocator({apple: 10, mango: 6, pineapple: 5}, [
+        {
+            name: "owd",
+            inventory: {apple: 5, mango: 3, pineapple: 3}
+        },
+        {
+            name: "dm",
+            inventory: {apple: 4, mango: 2, pineapple: 2}
+        },
+
+        {
+            name: "tm",
+            inventory: {pineapple: 2}
+        }
+
+    ])).toEqual([{
+        owd: {
+            pineapple: 3
+        }
+    },
+                    {
+                        dm: {
+                            pineapple: 2
+                        }
+                    },
+
+                ])
+})
+
+// Ship multiple items from multiple warehouses but none complete
+test('Multiple items multiple warehouses and none complete', () => {
+    expect(allocator({apple: 10, mango: 6, pineapple: 15}, [
+        {
+            name: "owd",
+            inventory: {apple: 5, mango: 3, pineapple: 3}
+        },
+        {
+            name: "dm",
+            inventory: {apple: 4, mango: 2, pineapple: 2}
+        },
+
+        {
+            name: "tm",
+            inventory: {pineapple: 2}
+        }
+
+    ])).toEqual([])
+})
+
+// Test whether empty incomplete warehouses are removed from the list
+test('Empty incomplete warehouses are removed from the list', () => {
+    expect(allocator({apple: 10, mango: 6, pineapple: 5}, [
+        {
+            name: "owd",
+            inventory: {apple: 5, mango: 3, pineapple: 3}
+        },
+        {
+            name: "dm",
+            inventory: {apple: 4}
+        },
+
+        {
+            name: "tm",
+            inventory: {pineapple: 2, mango: 3}
+        }
+
+    ])).toEqual(
+        [{
+            owd: {
+                mango: 3,
+                pineapple: 3
+            }
+        },
+            {
+                tm: {
+                    mango: 3,
+                    pineapple: 2
+                }
+            }])
+})
+
+// A random test
+test('Random test', () => {
+    // expect(add(1,3)).toBe(4)
+
+    expect(allocator({apple: 10}, [
+        {
+            name: "owd",
+            inventory: {apple: 5, mango: 3, pineapple: 3}
+        },
+        {
+            name: "dm",
+            inventory: {apple: 5}
+        }])).toEqual([
+                         {
+                             owd: {apple: 5}
+                         },
+                         {
+                             dm: {apple: 5}
+                         }
+
+                     ])
+})
--- inventory-allocator/src/InventoryAllocator.js
+++ inventory-allocator/src/InventoryAllocator.js
@@ -1,34 +1,18 @@
-let pineapple;
-const inventoryList = [
-    {
-        name: "owd",
-        inventory: {apple: 5, mango: 3, pineapple: 3}
-    },
-    {
-        name: "dm",
-        inventory: {apple: 5}
-    },
-    {
-        name: "ast",
-        inventory: {apple: 3, mango: 7, pineapple: 1}
-    },
-    {
-        name: "abc",
-        inventory: {apple: 5, mango: 3, pineapple: 6}
-    }
-]
-
-const requiredItems = {
-    apple: 10,
-    mango: 10,
-    pineapple: 10
-}
-
-allocator(inventoryList, requiredItems)
-
-function allocator(inventoryList, requiredItems) {
+/**
+ * This function computes the optimal quantity of items from different warehouses
+ * to complete a shipping order.
+ * @param requiredItems An object of items required by the order
+ * @param inventoryList The complete list of items available at each warehouse
+ * @returns {*|[]} An array of items from each warehouse that makes up the shipping
+ * order.
+ *
+ */
+function allocator(requiredItems, inventoryList) {
+    // The final list of shipment order
     let resultList = []
+    // To store the inventory from each warehouse
     let inventory = {}
+    // To store the name of each warehouse
     let name = ''
     for (const item of inventoryList) {
         name = item.name
@@ -41,14 +25,50 @@ function allocator(inventoryList, requiredItems) {
                     requiredItems[key] = 0
 
                 } else {
-                        inventory[key] = item.inventory[key]
-                        requiredItems[key] -= item.inventory[key]
-                        item.inventory[key] = 0
+                    inventory[key] = item.inventory[key]
+                    requiredItems[key] -= item.inventory[key]
+                    item.inventory[key] = 0
 
                 }
             }
+            if (requiredItems[key] === 0) {
+                delete requiredItems[key]
+            }
         }
-        resultList = [...resultList, {[name]: inventory}]
+
+        if (Object.keys(inventory).length !== 0) {
+            resultList = [...resultList, {[name]: inventory}]
+        }
+    }
+    resultList = removeUnfulfilledEntries(requiredItems, resultList)
+    resultList = removeEmptyArrayElements(resultList)
+    return resultList
+}
+
+// This function removes any partially complete items from the final list
+function removeUnfulfilledEntries(requiredItems, resultList) {
+    for (const [key, value] of Object.entries(requiredItems)) {
+        resultList.forEach(result => {
+                               const k = Object.keys(result)
+                               delete result[k][key]
+                           }
+        )
     }
     return resultList
 }
+
+// This function removes any empty properties from the final list
+function removeEmptyArrayElements(resultList) {
+    for (let key = 0; key < resultList.length; key++) {
+        const element = resultList[key]
+        for (const ele in element) {
+            if (JSON.stringify(element[ele]) === "{}") {
+                resultList.splice(key, 1)
+                key -= 1;
+            }
+        }
+    }
+    return resultList
+}
+
+module.exports = allocator;
